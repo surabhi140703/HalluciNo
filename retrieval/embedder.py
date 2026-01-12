3d42f4cdaef214fb892edd93974d6700556958af
surabhi140703 <surabhiv1407l@gmail.com>
2026-01-12 23:48:11 +1100

    added embedding and indexing logic

--- retrieval/embedder.py
+++ retrieval/embedder.py
@@ -10,3 +10,23 @@ def build_retriever(jsonl_path, model_name="all-MiniLM-L6-v2"):
         for line in f:
             chunks.append(json.loads(line))
 
+    texts= [c['text'] for c in chunks]
+    print("Encoding chunks")
+    model= SentenceTransformer(model_name)
+    embeddings= model.encode(texts, show_progress_bar=True)
+
+    #MiniLM dimension is 384
+    dimension= embeddings.shape[1]
+    faiss_index= faiss.IndexFlatIP(dimension)
+
+    faiss.normalize_L2(embeddings)
+    faiss_index.add(embeddings)
+
+    faiss.write_index(faiss_index, "papers_index.faiss")
+    np.save("embeddings.npy", embeddings)
+
+    print("Index and embeddings saved")
+    return model, faiss_index, chunks
+
+
+

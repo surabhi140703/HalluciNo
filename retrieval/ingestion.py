a813ef80d454492ee1c1853ca90e489a6b806764
surabhi140703 <surabhiv1407l@gmail.com>
2026-01-22 15:36:19 +1100

    changing chunking approach, redoing from ingestion.py

---
+++ retrieval/ingestion.py
@@ -0,0 +1,8 @@
+import os 
+import json
+import re
+import arxiv
+from pathlib import Path
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions, TableStructureOptions
